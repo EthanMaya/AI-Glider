79d38222a776291e4b40fc7f9cbd812d51b2f927
EthanMaya <92396506+EthanMaya@users.noreply.github.com>
2021-12-07 18:41:07 +0000

    Update and rename Pitch.py to PSEUDOCODE_Pitch.py

--- PSEUDOCODE_Pitch.py
+++ PSEUDOCODE_Pitch.py
@@ -1,3 +1,5 @@
+###THIS IS PSEUDOCODE FOR SIMPLE PROGRAMS TO CONTROL PHYSICAL GLIDERS###
+###WITH LITTLE EFFORT, THIS CAN BE TURNED INTO USABLE CODE###
 #pitch
 elovater = #output, control surface
 actual_pitch = #input gyro pitch of aircraft
